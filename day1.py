78cd1b570ee79316f908082af12238b990954c62
Jonathan Perret <j-github@jonathanperret.net>
2024-12-01 07:07:38 +0100

    day1 cleanup

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+from more_itertools import *
+
 SAMPLE = """3   4
 4   3
 2   5
@@ -5,19 +7,20 @@ SAMPLE = """3   4
 3   9
 3   3"""
 
+def parse(input):
+    lines = input.splitlines()
+    pairs = ([int(x) for x in line.split()] for line in lines)
+    left, right = (sorted(l) for l in unzip(pairs))
+    return left, right
 
 def part1(input):
-    lines = input.splitlines()
-    left = sorted([int(line.split()[0]) for line in lines])
-    right = sorted([int(line.split()[1]) for line in lines])
-    distances = [ abs(a - b) for a, b in zip(left, right)]
+    left, right = parse(input)
+    distances = [ abs(a - b) for a, b in zip(left, right) ]
     return sum(distances)
 
 def part2(input):
-    lines = input.splitlines()
-    left = sorted([int(line.split()[0]) for line in lines])
-    right = sorted([int(line.split()[1]) for line in lines])
-    scores = [ sum([ a for b in right if a == b ]) for a in left ]
+    left, right = parse(input)
+    scores = [ a * right.count(a) for a in left ]
     return sum(scores)
 
 INPUT = open("day1.txt", "r").read()
