081d79a226a4f5e9528092fa96224cc5e9649f4e
Jonathan Perret <j-github@jonathanperret.net>
2024-12-02 06:36:33 +0100

    day 2 cleanup

--- day2.py
+++ day2.py
@@ -13,39 +13,32 @@ def parse(input):
     levelrows = ([int(x) for x in line.split()] for line in lines)
     return levelrows
 
+def checklevels(levels):
+    deltas = [ (b - a) for a, b in pairwise(levels) ]
+    ok = (all((abs(d) >=1 and abs(d) <=3) for d in deltas)
+        and
+        (all(d <= 0 for d in deltas) or all(d >= 0 for d in deltas))
+    )
+    return ok
+
 def part1(input):
     levelrows = parse(input)
-    okcount = 0
-    for levels in levelrows:
-        deltas = [ (b - a) for a, b in pairwise(levels) ]
-        ok = (all((abs(d) >=1 and abs(d) <=3) for d in deltas)
-            and
-            (all(d <= 0 for d in deltas) or all(d >= 0 for d in deltas))
-        )
-        print(deltas, ok)
-        if ok:
-            okcount += 1
-    return okcount
+    return quantify(levelrows, checklevels)
+
+def except_nth(it, n):
+    return (x for i, x in zip(count(), it) if i is not n)
 
 def part2(input):
     levelrows = parse(input)
-    okcount = 0
-    for levels in levelrows:
-        for skip in range(len(levels) + 1):
-            partial = [l for i, l in zip(count(), levels) if i is not skip]
-            deltas = [ (b - a) for a, b in pairwise(partial) ]
-            ok = (all((abs(d) >=1 and abs(d) <=3) for d in deltas)
-                and
-                (all(d <= 0 for d in deltas) or all(d >= 0 for d in deltas))
-            )
-            if ok:
-                okcount += 1
-                break
-    return okcount
+    return quantify(
+        any(checklevels(except_nth(levels, skip))
+            for skip in range(len(levels) + 1))
+        for levels in levelrows
+    )
 
 INPUT = open("day2.txt", "r").read()
 
 print("part1:", part1(SAMPLE))
-#print("part1:", part1(INPUT))
+print("part1:", part1(INPUT))
 print("part2:", part2(SAMPLE))
 print("part2:", part2(INPUT))
