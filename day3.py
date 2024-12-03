579990bdb6a2428bf1ef0c6dbf1bc49bb7f46da5
Jonathan Perret <j-github@jonathanperret.net>
2024-12-03 06:39:35 +0100

    day 3 cleanup

--- day3.py
+++ day3.py
@@ -6,23 +6,23 @@ SAMPLE = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5)
 SAMPLE2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 def part1(input):
-    pat = re.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)")
+    pat = re.compile(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)")
     matches = pat.findall(input)
     products = [ int(a)*int(b) for a,b in matches]
     return sum(products)
 
 def part2(input):
-    pat = re.compile("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|don'(t)\\(\\)|(d)o\\(\\)")
+    pat = re.compile(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|(don't)\(\)|(do)\(\)")
     matches = pat.findall(input)
     enabled = True
     result = 0
-    for m in matches:
-        if m[2] == 't':
+    for a, b, off, on in matches:
+        if off:
             enabled = False
-        elif m[3] == 'd':
+        elif on:
             enabled = True
         elif enabled:
-            result += int(m[0]) * int(m[1])
+            result += int(a) * int(b)
     return result
 
 INPUT = open("day3.txt", "r").read()
