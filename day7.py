09d6a0d8036afd5f5f7a750465a2c26c9b5b2c62
Jonathan Perret <j-github@jonathanperret.net>
2024-12-07 07:31:35 +0100

    day 7 cleanup

--- day7.py
+++ day7.py
@@ -14,52 +14,63 @@ SAMPLE = """190: 10 19
 292: 11 6 16 20
 """
 
-def part1(input):
+def parse(input):
     lines = input.splitlines()
-    equations = [ list(map(int, re.split(':? ', line))) for line in lines ]
-    result = 0
-    for equation in equations:
-        print(equation)
-        for bits in range(2 ** (len(equation) - 2)):
-            target, total, *nums = equation
-            for bit, num in enumerate(nums):
-                if bits & (1 << bit):
-                    total += num
-                else:
-                    total *= num
-            if total == target:
-                result += target
-                break
-    return result
+    return [ list(map(int, re.split(':? ', line))) for line in lines ]
+
+def solve(input, allow_concat):
+    equations = parse(input)
+
+    def check(target, nums):
+        if len(nums) == 1:
+            return nums[0] == target
+
+        *nums, lastnum = nums
+
+        if target < lastnum:
+            return False
+
+        # +
+        if check(target - lastnum, nums):
+            return True
+
+        # *
+        q, r = divmod(target, lastnum)
+        if r == 0 and check(q, nums):
+            return True
+
+        # ||
+        if allow_concat and target != lastnum:
+            left = int(str(target).removesuffix(str(lastnum)))
+            if left < target and check(left, nums):
+                return True
+
+        return False
+
+    return sum(target
+               for target, *nums in equations
+               if check(target, nums))
+
+def part1(input):
+    return solve(input, allow_concat=False)
 
 def part2(input):
-    lines = input.splitlines()
-    equations = [ list(map(int, re.split(':? ', line))) for line in lines ]
-    result = 0
-    for equation in equations:
-        print(equation)
-        for bits in range(3 ** (len(equation) - 2)):
-            target, total, *nums = equation
-            rest = bits
-            # print("trying", bits)
-            for num in nums:
-                rest, op = divmod(rest, 3)
-                if op == 0:
-                    total += num
-                if op == 1:
-                    total *= num
-                elif op == 2:
-                    #print(f"{total} || {num} -> {int(str(total) + str(num))}")
-                    total = int(str(total) + str(num))
-            if total == target:
-                print("ok")
-                result += target
-                break
-    return result
+    return solve(input, allow_concat=True)
 
 INPUT = open("day7.txt", "r").read()
 
-# print("part1:", part1(SAMPLE))
-# print("part1:", part1(INPUT))
-print("part2:", part2(SAMPLE))
-print("part2:", part2(INPUT))
+result = part1(SAMPLE)
+print("part1:", result)
+assert result == 3749
+
+result = part1(INPUT)
+print("part1:", result)
+assert result == 4122618559853
+
+result = part2(SAMPLE)
+print("part2:", result)
+assert result == 11387
+
+result = part2(INPUT)
+print("part2:", result)
+assert result == 227615740238334
