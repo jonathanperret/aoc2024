e55159e150c0ed2eea461b129563e8eeb51524c7
Jonathan Perret <j-github@jonathanperret.net>
2024-12-12 07:09:31 +0100

    day 12 cleanup

--- day12.py
+++ day12.py
@@ -23,93 +23,59 @@ MMMISSJEEE
 def neighbors(i, j):
     return [(i-1, j), (i, j-1), (i, j+1), (i+1, j)]
 
-def part1(input):
-    matrix = input.splitlines()
+
+def flood(matrix, start):
     height = len(matrix)
     width = len(matrix[0])
-    visited = set()
-    regions = []
-    result = 0
-    for i, row in enumerate(matrix):
-        for j, cell in enumerate(row):
-            if (i,j) in visited:
+    region = {start}
+    edges = set()
+    i, j = start
+    cell = matrix[i][j]
+    frontier = [start]
+    while len(frontier) > 0:
+        i1, j1 = frontier.pop()
+        for d, (i2, j2) in enumerate(neighbors(i1, j1)):
+            if (i2, j2) in region:
                 continue
-            visited.add((i, j))
-            region = set([(i, j)])
-            edge = [(i, j)]
-            perimeter = 0
-            while len(edge) > 0:
-                i1, j1 = edge.pop()
-                for i2, j2 in neighbors(i1, j1):
-                    if i2 < 0 or i2 >= height \
-                       or j2 < 0 or j2 >= width:
-                        perimeter += 1
-                        continue
-                    if (i2, j2) in region:
-                        continue
-                    cell2 = matrix[i2][j2]
-                    if cell == cell2:
-                        visited.add((i2, j2))
-                        region.add((i2, j2))
-                        edge.append((i2, j2))
-                    else:
-                        perimeter += 1
-            result += len(region) * perimeter
+            if i2 >= 0 and i2 < height \
+               and j2 >= 0 and j2 < width \
+               and matrix[i2][j2] == cell:
+                region.add((i2, j2))
+                frontier.append((i2, j2))
+            else:
+                edges.add((d, (i1, j1)))
+    return region, edges
 
-    return result
+
+def find_regions(input):
+    matrix = input.splitlines()
+    visited = set()
+    result = 0
+    regions = []
+    for i, j in product(range(len(matrix)), range(len(matrix[0]))):
+        if (i,j) in visited:
+            continue
+        region, edges = flood(matrix, (i, j))
+        regions.append((region, edges))
+        visited |= region
+    return regions
+
+
+def part1(input):
+    return sum(len(region) * len(edges)
+               for region, edges in find_regions(input))
+
 
 def part2(input):
-    matrix = input.splitlines()
-    height = len(matrix)
-    width = len(matrix[0])
-    visited = set()
-    regions = []
     result = 0
-    for i, row in enumerate(matrix):
-        for j, cell in enumerate(row):
-            if (i,j) in visited:
-                continue
-            visited.add((i, j))
-            region = set([(i, j)])
-            frontier = [(i, j)]
-            edges = []
-            perimeter = 0
-            while len(frontier) > 0:
-                i1, j1 = frontier.pop()
-                for d, (i2, j2) in enumerate(neighbors(i1, j1)):
-                    if i2 < 0 or i2 >= height \
-                       or j2 < 0 or j2 >= width:
-                        edges.append((d, (i1, j1)))
-                        perimeter += 1
-                        continue
-                    if (i2, j2) in region:
-                        continue
-                    cell2 = matrix[i2][j2]
-                    if cell == cell2:
-                        visited.add((i2, j2))
-                        region.add((i2, j2))
-                        frontier.append((i2, j2))
-                    else:
-                        edges.append((d, (i1, j1)))
-                        perimeter += 1
-            edgecount = 0
-            print(edges)
-            for d, (i1, j1) in edges:
-                i2, j2 = (i1, j1 - 1) if d in [0, 3] else (i1 - 1, j1)
-                print(f"looking at {(i2, j2)} from {(d, (i1, j1))}")
-                if i2 < 0 or i2 >= height \
-                   or j2 < 0 or j2 >= width:
-                    print("outside")
-                    edgecount += 1
-                    continue
-                if (d, (i2, j2)) in edges:
-                    print("skipping", (d, (i2, j2)), "from", (d, (i1, j1)))
-                    continue
+    for region, edges in find_regions(input):
+        edgecount = 0
+        for d, (i1, j1) in edges:
+            i2, j2 = (i1, j1 - 1) if d in [0, 3] else (i1 - 1, j1)
+            if (d, (i2, j2)) not in edges:
                 edgecount += 1
-            print(edgecount)
 
-            result += len(region) * edgecount
-            regions.append(region)
+        result += len(region) * edgecount
 
     return result
 
@@ -122,7 +88,7 @@ if __name__ == '__main__':
 
     result = part2(INPUT)
     print("part2:", result)
-    #assert result == 224577979481346
+    assert result == 844198
 
-    #num, total = timeit.Timer(lambda: part2(INPUT)).autorange()
-    #print("time=", total / num)
+    num, total = timeit.Timer(lambda: part2(INPUT)).autorange()
+    print("time=", total / num)
