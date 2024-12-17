7173a9318a08a1327b33c2512a099c938ff2d82d
Jonathan Perret <j-github@jonathanperret.net>
2024-12-17 07:33:49 +0100

    day 17

---
+++ day17.py
@@ -0,0 +1,169 @@
+from itertools import *
+from more_itertools import *
+import re
+from functools import partial, cmp_to_key
+from collections import defaultdict
+from math import gcd, sqrt
+from heapq import *
+import timeit
+import functools
+
+SAMPLE = """Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0
+"""
+
+SAMPLE2 = """Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0
+"""
+
+OPCODES = [ 'adv', 'bxl', 'bst', 'jnz', 'bxc', 'out', 'bdv', 'cdv' ]
+
+def part1(input):
+    ra, rb, rc, *program = map(int, re.findall(r"\d+", input))
+    output = []
+    pc = 0
+    argstr = ['']
+    def combo(arg):
+        if arg < 4:
+            return arg
+        argstr[0] = 'abc'[arg - 4]
+        return [ra, rb, rc][arg - 4]
+
+    while pc < len(program) - 1:
+        opcode = program[pc]
+        arg = program[pc + 1]
+        argstr[0] = str(arg)
+        pc += 2
+        match opcode:
+            case 0:
+                ra //= 2 ** combo(arg)
+            case 1:
+                rb ^= arg
+            case 2:
+                rb = combo(arg) % 8
+            case 3:
+                if ra != 0:
+                    pc = arg
+            case 4:
+                rb ^= rc
+            case 5:
+                output.append(str(combo(arg) % 8))
+            case 6:
+                rb = ra // (2 ** combo(arg))
+            case 7:
+                rc = ra // (2 ** combo(arg))
+            case _:
+                print(f"unimplemented {opcode}")
+                pass
+        print(f"{OPCODES[opcode]} {argstr[0]} -> pc={pc} ra={ra} rb={rb} rc={rc}")
+
+    return (','.join(output))
+
+
+def part2(input):
+    _, rb0, rc0, *program = map(int, re.findall(r"\d+", input))
+
+    def run(ra, rb, rc):
+        output = []
+        pc = 0
+        argstr = ['']
+        def combo(arg):
+            if arg < 4:
+                return arg
+            argstr[0] = 'abc'[arg - 4]
+            return [ra, rb, rc][arg - 4]
+
+        while pc < len(program) - 1:
+            opcode = program[pc]
+            arg = program[pc + 1]
+            argstr[0] = str(arg)
+            pc += 2
+            match opcode:
+                case 0:
+                    ra //= 2 ** combo(arg)
+                case 1:
+                    rb ^= arg
+                case 2:
+                    rb = combo(arg) % 8
+                case 3:
+                    if ra != 0:
+                        pc = arg
+                case 4:
+                    rb ^= rc
+                case 5:
+                    output.append(combo(arg) % 8)
+                    if output[-1] != program[len(output)-1]:
+                        print(f" FAIL: {output[-1]} instead of {program[len(output)-1]}")
+                        return False
+                case 6:
+                    rb = ra // (2 ** combo(arg))
+                case 7:
+                    rc = ra // (2 ** combo(arg))
+                case _:
+                    raise Exception(f"unimplemented {opcode}")
+
+            print(f" {OPCODES[opcode]} {argstr[0]} -> pc={pc} ra={ra} rb={rb} rc={rc}")
+        if output == program:
+            print(" FOUND")
+            return True
+
+
+    if program == [2,4,1,3,7,5,1,5,0,3,4,3,5,5,3,0]:
+        print("cheating")
+        candidates = (a << 33 | b
+                      for a in range(100000000)
+                      for b in [0o66052247155, 0o66052247277])
+        for ra0 in candidates:
+            fail = False
+            ra = ra0
+            for i, op in enumerate(program):
+                out = (((ra & 7) ^ 6) ^ (ra >> ((ra & 7) ^ 3))) & 7
+                ra = ra >> 3
+                if out != op:
+                    if i > 13:
+                        print(f"{oct(ra0)} fails at {i}")
+                    fail = True
+                    break
+            if not fail:
+                print(f"found {ra0} ({oct(ra0)})")
+                break
+
+        return ra0
+
+    for ra0 in range(1000000):
+        print(f"TESTING {ra0}...")
+
+        if run(ra0, rb0, rc0):
+            break
+
+    return ra0
+
+
+def test_part1():
+    assert part1(SAMPLE) == '4,6,3,5,6,3,5,2,1,0'
+
+
+def test_part2():
+    assert part2(SAMPLE2) == 117440
+
+
+if __name__ == '__main__':
+    inputfile = re.sub(r"^.*?([^/]+)\.py$", r"\1.txt", __file__)
+    INPUT = open(inputfile, "r").read()
+
+    result = part1(INPUT)
+    print("part1:", result)
+    assert result == '6,2,7,2,3,1,6,0,5'
+
+    result = part2(INPUT)
+    print("part2:", result)
+    #assert result == 575
+
+    #num, total = timeit.Timer(lambda: part2(INPUT)).autorange()
+    #print("time=", total / num)
