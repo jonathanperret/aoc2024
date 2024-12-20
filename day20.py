eb9a10d658487a2eba3e4763ce02eff44572f4cc
Jonathan Perret <j-github@jonathanperret.net>
2024-12-20 07:09:11 +0100

    day 20 failing

--- day20.py
+++ day20.py
@@ -27,17 +27,17 @@ SAMPLE = """###############
 """
 
 
-def part1(input, limit=100):
+def solve(input, max_cheat_len=20, limit=100):
     matrix = [ list(line) for line in input.splitlines() ]
     walls = { (i,j)
              for i, row in enumerate(matrix)
              for j, cell in enumerate(row)
-             if matrix[i][j] == '#' }
+             if cell == '#' }
     for i, row in enumerate(matrix):
         for j, cell in enumerate(row):
-            if matrix[i][j] == 'S':
+            if cell == 'S':
                 start = (i,j)
-            elif matrix[i][j] == 'E':
+            elif cell == 'E':
                 end = (i,j)
 
     g = nx.grid_2d_graph(len(matrix), len(matrix[0]))
@@ -49,22 +49,40 @@ def part1(input, limit=100):
     max_len = nx.shortest_path_length(g, start, end)
     print("non-cheating:", max_len)
 
+
+    paths_from_start = nx.single_source_shortest_path_length(g, start)
+    # print(f"paths_from_start: {paths_from_start}")
+    paths_to_end = nx.single_source_shortest_path_length(g, end)
+    # print(f"paths_to_end: {paths_to_end}")
+
     cheats = defaultdict(set)
     for i, row in enumerate(matrix):
         for j, cell in enumerate(row):
             if cell != '#':
-                for new_pos in [(i+2,j), (i,j+2)]:
-                    try:
-                        new_cell = matrix[new_pos[0]][new_pos[1]]
-                    except IndexError:
-                        continue
-                    if new_cell != '#':
-                        g.add_edge((i,j), new_pos)
-                        min_len = nx.shortest_path_length(g, start, end)
-                        saved = max_len-min_len-1
-                        cheats[saved].add(((i,j), new_pos))
-                        print(i, j, saved)
-                        g.remove_edge((i,j), new_pos)
+                for di in range(max_cheat_len + 1):
+                    for dj in range(max_cheat_len + 1 - di):
+                        try:
+                            new_cell = matrix[i+di][j+dj]
+                        except IndexError:
+                            continue
+                        if new_cell != '#':
+                            new_pos = (i+di, j+dj)
+
+                            to_cell = paths_from_start[(i,j)]
+                            rest = paths_to_end[new_pos]
+                            total = to_cell + di + dj + rest
+                            saved = max_len - total
+                            if saved > 0:
+                                cheats[saved].add(((i,j), new_pos))
+
+                            to_cell = paths_from_start[new_pos]
+                            rest = paths_to_end[(i,j)]
+                            total = to_cell + di + dj + rest
+                            saved = max_len - total
+                            if saved > 0:
+                                cheats[saved].add(((i,j), new_pos))
+
+
 
     result = 0
     for saved in sorted(cheats.keys()):
@@ -75,8 +93,12 @@ def part1(input, limit=100):
     return result
 
 
-def part2(input):
-    return 2
+def part1(input, limit = 100):
+    return solve(input, max_cheat_len=2, limit=limit)
+
+
+def part2(input, limit = 100):
+    return solve(input, max_cheat_len=20, limit=limit)
 
 
 def test_part1():
@@ -84,7 +106,7 @@ def test_part1():
 
 
 def test_part2():
-    assert part2(SAMPLE) == 2
+    assert part2(SAMPLE, 74) == 7
 
 
 if __name__ == '__main__':
@@ -95,8 +117,10 @@ if __name__ == '__main__':
     print("part1:", result)
     assert result == 1521
 
-    #result = part2(INPUT)
-    #print("part2:", result)
+    result = part2(INPUT)
+    print("part2:", result)
+    # 266594 too low
+    # 558948 too low
     #assert result == 724388733465031
 
     #num, total = timeit.Timer(lambda: part2(INPUT)).autorange()
