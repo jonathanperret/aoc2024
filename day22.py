07e0956dd2d82a8675f5cc2a0f16088898f40fba
Jonathan Perret <j-github@jonathanperret.net>
2024-12-22 08:00:09 +0100

    day 22 part 2

--- day22.py
+++ day22.py
@@ -15,6 +15,12 @@ SAMPLE = """1
 2024
 """
 
+SAMPLE2 = """1
+2
+3
+2024
+"""
+
 def make_secret(secret, rounds):
     for _ in range(rounds):
         secret = ((secret << 6) ^ secret) & 0xffffff
@@ -22,6 +28,20 @@ def make_secret(secret, rounds):
         secret = ((secret << 11) ^ secret) & 0xffffff
     return secret
 
+def find_period():
+    secret = seed = 16777215
+    last = 0
+    gen = set([secret])
+    for i in range(16777217):
+        secret = ((secret << 6) ^ secret) & 0xffffff
+        secret = ((secret >> 5) ^ secret)
+        secret = ((secret << 11) ^ secret) & 0xffffff
+        gen.add(secret)
+        if secret == seed:
+            print(i, len(gen))
+            break
+
+# find_period()
 
 def part1(input):
     seeds = map(int, input.splitlines())
@@ -31,9 +51,89 @@ def part1(input):
         result += secret
     return result
 
+def xorshift(secret):
+    secret = ((secret << 6) ^ secret) & 0xffffff
+    secret = ((secret >> 5) ^ secret) & 0xffffff
+    secret = ((secret << 11) ^ secret) & 0xffffff
+    return secret
+
+
+def checksecret(trace, secret):
+    testtrace = 0
+    for _ in range(10):
+        secret = xorshift(secret)
+        testtrace = testtrace * 10 + secret % 10
+        if testtrace == trace:
+            return
+    raise Exception(f"expected {testtrace} == {trace}")
+
+def findsecrets(traces):
+    traceset = set(traces)
+    mask = 10**8
+    secret = last = 1
+    secretdict = {}
+    secretseq = []
+    for i in range(16777217 + 1000):
+        secretseq.append(secret)
+        secret = ((secret << 6) ^ secret) & 0xffffff
+        secret = ((secret >> 5) ^ secret) & 0xffffff
+        secret = ((secret << 11) ^ secret) & 0xffffff
+        last = (last * 10 + (secret % 10)) % mask
+        if last in traceset:
+            secretdict[last] = secretseq[i - 7]
+            traceset.remove(last)
+        elif (last % 10000000) in traceset:
+            secretdict[last % 10000000] = secretseq[i - 6]
+            traceset.remove(last % 10000000)
+        elif (last % 1000000) in traceset:
+            secretdict[last % 1000000] = secretseq[i - 5]
+            traceset.remove(last % 1000000)
+        else:
+            continue
+        # print(i, last, secret)
+
+    print(f"found {len(secretdict)} secrets, remaining {len(traceset)}")
+
+    for trace, secret in secretdict.items():
+        checksecret(trace, secret)
+
+    return secretdict
+
+
+def optimize(secrets):
+    seqdicts = []
+    allseqs = set()
+    for monkey, secret in enumerate(secrets):
+        prices = []
+        seq = []
+        seqdict = defaultdict(lambda: 0)
+        price = secret % 10
+        for i in range(2000):
+            secret = xorshift(secret)
+            old, price = price, secret % 10
+            seq.append(price - old)
+            if len(seq) > 4:
+                seq[:1] = []
+            if len(seq) == 4:
+                seqt = tuple(seq)
+                if seqt not in seqdict:
+                    seqdict[seqt] = price
+                    allseqs.add(seqt)
+        seqdicts.append(seqdict)
+
+    print(f"found {len(allseqs)} different sequences")
+
+    grosses = (sum(seqdict[seq]
+                    for seqdict in seqdicts)
+               for seq in allseqs)
+
+    return max(grosses)
+
 
 def part2(input):
-    return 2
+    secrets = map(int, input.splitlines())
+
+    return optimize(secrets)
 
 
 def test_part1():
@@ -41,7 +141,7 @@ def test_part1():
 
 
 def test_part2():
-    assert part2(SAMPLE) == 2
+    assert part2(SAMPLE2) == 23
 
 
 if __name__ == '__main__':
@@ -52,8 +152,8 @@ if __name__ == '__main__':
     print("part1:", result)
     assert result == 13753970725
 
-    # result = part2(INPUT)
-    # print("part2:", result)
+    result = part2(INPUT)
+    print("part2:", result)
     # assert result == 170279148659464
 
     # num, total = timeit.Timer(lambda: part2(INPUT)).autorange()
